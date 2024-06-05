02d2605f5c85296dfecc5d4bf5f2c2af256d2ccb
Alejandro Elias <alejandroelias25@gmail.com>
2024-06-05 22:38:43 +0000

    cambia variables de entorno

--- myApp.js
+++ myApp.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const uri = process.env.MONGO_URI
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.3gunrw3.mongodb.net/boilerpalte?retryWrites=true&w=majority&appName=Cluster0`;
 
-mongoose.connect( uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB Atlas'))
-    .catch(err => console.error('Error connecting to MongoDB Atlas:', err))
+mongoose.connect(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    auth: {
+        user: process.env.MONGO_USER,
+        password: process.env.MONGO_PASSWORD
+    }
+})
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch(err => console.error('Error connecting to MongoDB Atlas:', err));
