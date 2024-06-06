ba3d7f669101a6f6a9c74277901247ceb7a7ef0d
Alejandro Elias <alejandroelias25@gmail.com>
2024-06-06 01:24:06 +0000

    cambia direccion Atlas

--- myApp.js
+++ myApp.js
@@ -1,15 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.3gunrw3.mongodb.net/boilerpalte?retryWrites=true&w=majority&appName=Cluster0`;
+console.log('MONGO_URI:', process.env.MONGO_URI);
 
-mongoose.connect(uri, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    auth: {
-        user: process.env.MONGO_USER,
-        password: process.env.MONGO_PASSWORD
-    }
-})
-.then(() => console.log('Connected to MongoDB Atlas'))
-.catch(err => console.error('Error connecting to MongoDB Atlas:', err));
+mongoose.connect( process.env.MONGO_URI , { useNewUrlParser: true, useUnifiedTopology: true })
